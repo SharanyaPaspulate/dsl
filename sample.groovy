56a61799871ef1f9354ba3cb2a2a6476fafd9136
sharanya <sharanyapaspulate@gmail.com>
2017-01-18 20:38:04 +0530

    added changes

--- sample.groovy
+++ sample.groovy
@@ -24,7 +24,7 @@ freeStyleJob('sample_dsl') {
       shell("echo hello world")
     }
     postBuildSteps {
-        buildFlowJob(String "sample", Closure closure  = null)
+        job(String sample, Closure closure  = null)
     configure { it <<
         'runPostStepsIfResult' {
             name('SUCCESS')
