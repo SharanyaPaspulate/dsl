de16dd0bce616b54550060a61a6d07861c976170
sharanya <sharanyapaspulate@gmail.com>
2017-01-18 12:58:18 +0530

    added another job

--- sample.groovy
+++ sample.groovy
@@ -21,3 +21,28 @@ freeStyleJob('sample_dsl') {
       shell("echo hello world")
     }
 }
+
+
+freeStyleJob('sample') {
+    
+    description("Do not change this config directly.")
+    label 'linux'
+    logRotator(-1, 10000, 5, -1)
+    scm {
+        git {
+            remote {
+                url("https://github.com/SharanyaPaspulate/dsl.git")
+            }
+            branch '*/master'
+        }
+    }
+    // antInstallation('Ant-1.9.0')
+    triggers {
+        githubPush()
+        scm("* * * * *")
+    }
+
+    steps {
+      shell("echo hello SharanyaPaspulate")
+    }
+}
